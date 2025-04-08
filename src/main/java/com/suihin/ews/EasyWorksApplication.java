5c7af1f36783987f82fc60cd375a3b317d06bf8a
tnqls847 <tnqls3718@gmail.com>
2025-04-08 19:06:54 +0900

    최초 커밋

---
+++ src/main/java/com/suihin/ews/EasyWorksApplication.java
@@ -0,0 +1,13 @@
+package com.suihin.ews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyWorksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyWorksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/suihin/ews/EasyWorksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suihin.ews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyWorksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
